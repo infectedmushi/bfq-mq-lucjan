39a3e2fd7b24efb212826b21f1333ba9311dd4ea
Piotr Gorski <lucjan.lucjanov@gmail.com>
2022-01-22 01:47:30 +0100

    elevator: always use bfq unless overridden by flag
    Signed-off-by: Piotr Gorski <lucjan.lucjanov@gmail.com>

--- block/elevator.c
+++ block/elevator.c
@@ -616,15 +616,21 @@ static inline bool elv_support_iosched(struct request_queue *q)
 }
 
 /*
- * For single queue devices, default to using bfq. If we have multiple
- * queues or bfq is not available, default to "none".
+ * Always use bfq as our elevator if possible. If bfq is unavailable default
+ * to mq-deadline for single-queue and none for multi-queue devices.
  */
 static struct elevator_type *elevator_get_default(struct request_queue *q)
 {
+#ifndef CONFIG_IOSCHED_BFQ    
 	if (q->nr_hw_queues != 1)
 		return NULL;
+#endif
 
+#if defined(CONFIG_IOSCHED_BFQ)
 	return elevator_get(q, "bfq", false);
+#else
+	return elevator_get(q, "mq-deadline", false);
+#endif
 }
 
 /*
